3464282ec39c7d7b851bf5827952b463ecd58626
linbbc <1376122433@qq.com>
2021-07-30 17:54:49 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld :msg="msg" />
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      msg: {
-        value: "Hello Vue 3 + TypeScript + Vite",
-        color: "green" //00a223
-      }
-    }
-  }
-})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
